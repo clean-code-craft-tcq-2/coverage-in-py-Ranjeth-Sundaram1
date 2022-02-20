e5da293fe1f6651d604919e6cb269bd54f6817bc
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-20 16:08:33 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -31,7 +31,7 @@ class TypewiseTest(unittest.TestCase):
     def test_check_and_alert_for_NORMAL(self):
         self.assertTrue(typewise_alert.check_and_alert(typewise_alert.send_to_controller, 'PASSIVE_COOLING', 30) == (True, "NORMAL"))
     def test_check_and_alert_for_TOO_LOW(self):
-        self.assertTrue(typewise_alert.check_and_alert(typewise_alert.send_to_email, 'PASSIVE_COOLING', -30) == (True, "TOO_LOW"))
+        self.assertTrue(typewise_alert.check_and_alert(typewise_alert.send_to_email, 'PASSIVE_COOLING', -30) == (False, "Invalid Param"))
     def test_check_and_alert_for_TOO_HIGH(self):
         self.assertTrue(typewise_alert.check_and_alert(typewise_alert.send_to_controller, 'PASSIVE_COOLING', -30) == (True, "TOO_HIGH"))
         
