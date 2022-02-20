757d0750c7b8803b85d97bef4057d3122063bab8
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-20 16:44:23 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -15,11 +15,11 @@ def DefineCoolingtype_limits(coolingType):
         return({"lowerLimit" : 'NA', "upperLimit" : 'NA'})
 
 def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
-  if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
+    if value < lowerLimit:
+        return 'TOO_LOW'
+    if value > upperLimit:
+        return 'TOO_HIGH'
+    return 'NORMAL'
 
 def classify_temperature_breach(coolingType, temperatureInC):
     limits  = DefineCoolingtype_limits(coolingType)
@@ -30,10 +30,11 @@ def classify_temperature_breach(coolingType, temperatureInC):
 
 def IsbatteryCharValid(batteryChar):
     batteryChar_types = ['PASSIVE_COOLING', 'HI_ACTIVE_COOLING', 'MED_ACTIVE_COOLING']
+    print(batteryChar)
     if batteryChar in batteryChar_types:
         return True
     return False
-  
+
 def GetBreachType(batteryChar, temperatureInC):
     breachType =  classify_temperature_breach(batteryChar, temperatureInC) if IsbatteryCharValid(batteryChar) else False
     return breachType if breachType!=False else 'Invalid_Param'
@@ -45,8 +46,10 @@ def check_and_alert(alertTarget, batteryChar, temperatureInC):
     return((alert_status, breachType))
 
 def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
+    header = 0xfeed
+    command_to_controller = (f'{header}, {breachType}')
+    print(command_to_controller)
+    return(True)
 
 def Generate_email_content(breachtype, email_messages):
     return email_messages[breachtype]
@@ -55,4 +58,4 @@ def send_to_email(breachType):
     mail_content = Generate_email_content(breachType, email_info['messages'])
     sent_email = f"To: {email_info['recepient']} : {mail_content}"
     print(sent_email)
-    return(sent_email)
+    return(True)
