0247f8af5cf66a47df4aee54ce55145fb1468407
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-20 14:41:48 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -9,13 +9,13 @@ class TypewiseTest(unittest.TestCase):
     def test_Generate_email_content_for_high(self):
         self.assertTrue(typewise_alert.Generate_email_content('TOO_HIGH', typewise_alert.email_info['messages']) == 'Hi, the temperature is too high')
     def test_DefineCoolingtype_limits_for_PASSIVE_COOLING(self):
-        self.assertTrue(typewise_alert.DefineCoolingtype_limits('PASSIVE_COOLING') == 0, 35)
+        self.assertTrue(typewise_alert.DefineCoolingtype_limits('PASSIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 35})
     def test_DefineCoolingtype_limits_for_HI_ACTIVE_COOLING(self):
-        self.assertTrue(typewise_alert.DefineCoolingtype_limits('HI_ACTIVE_COOLING') == 0, 45)
+        self.assertTrue(typewise_alert.DefineCoolingtype_limits('HI_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 45})
     def test_DefineCoolingtype_limits_for_MED_ACTIVE_COOLING(self):
-        self.assertTrue(typewise_alert.DefineCoolingtype_limits('MED_ACTIVE_COOLING') == 0, 40)
+        self.assertTrue(typewise_alert.DefineCoolingtype_limits('MED_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 40})
     def test_DefineCoolingtype_limits_for_Wrong_key(self):
-        self.assertTrue(typewise_alert.DefineCoolingtype_limits('WRONG_KEY') == 0, 0)
+        self.assertTrue(typewise_alert.DefineCoolingtype_limits('WRONG_KEY') == {"lowerLimit" : 'NA', "upperLimit" : 'NA'})
 
 if __name__ == '__main__':
   unittest.main()
