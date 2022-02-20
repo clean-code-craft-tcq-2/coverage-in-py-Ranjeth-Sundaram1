020fd7f8dcc43053eb413895ba1772fc583bd104
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-02-20 15:01:26 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -16,6 +16,12 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(typewise_alert.DefineCoolingtype_limits('MED_ACTIVE_COOLING') == {"lowerLimit" : 0, "upperLimit" : 40})
     def test_DefineCoolingtype_limits_for_Wrong_key(self):
         self.assertTrue(typewise_alert.DefineCoolingtype_limits('WRONG_KEY') == {"lowerLimit" : 'NA', "upperLimit" : 'NA'})
-
+    def test_classify_temperature_breach_for_NORMAL(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 38)=='NORMAL')
+    def test_classify_temperature_breach_for_TOO_HIGH(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 50)=='TOO_HIGH')
+    def test_classify_temperature_breach_for_TOO_LOW(self):
+        self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 30)=='TOO_LOW')
+        
 if __name__ == '__main__':
   unittest.main()
